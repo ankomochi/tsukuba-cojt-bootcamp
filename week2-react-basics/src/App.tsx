1bb75f058268feacbeb9857eec00e5a8d0cf277b
hikapi <hikaru.un.yoshimori@gmail.com>
2025-04-30 14:49:20 +0900

    Reactの練習2

--- week2-react-basics/src/App.tsx
+++ week2-react-basics/src/App.tsx
@@ -1,19 +1,36 @@
 import { useState } from 'react';
 import './index.css';
+import Message from './Message';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [countAll, setCountAll] = useState(0);
 
   return (
     <div className="App">
       <h1>React カウンター</h1>
+      <h2>ボタンを押してカウントしよう</h2>
       <p>カウント: {count}</p>
-      <button onClick={() => setCount(count + 1)}>
-      増やす！
+      <button onClick={() => 
+        {
+          setCount(count - 1);
+          setCountAll(countAll + 1);
+        }}>
+      減らす↓
       </button>
-      {count === 5 && <p>5回クリックしたよ！</p>}
-      {count === 10 && <p>10回クリックしたよ！</p>}
+      <button onClick={() => 
+        {
+          setCount(count + 1);
+          setCountAll(countAll + 1);
+        }}>
+      増やす↑
+      </button>
+      {count === 5 && <p>+5だよ！</p>}
+      {count === 10 && <p>+10だよ！</p>}
+      <Message hoge="ホゲホゲ" text="これは子コンポーネントです！" hikaru="ひかるです"/>
+      <p class="allcount">全てのカウント(ボタンを押した回数): {countAll}</p>
     </div>
+    
   );
 }
 
---
+++ week2-react-basics/src/Message.tsx
@@ -0,0 +1,11 @@
+function Message(props){
+    return  (
+        <>
+            <p>{props.text}</p>
+            <p>{props.hoge}</p>
+            <p>{props.hikaru}</p>
+        </>
+    );
+}
+
+export default Message;
